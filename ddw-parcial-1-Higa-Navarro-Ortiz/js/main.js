7411cccbde421324b05868f2d32f59bcb81f73f3
Ivan Higa <ivzeq.higa@gmail.com>
2024-11-17 19:33:31 -0300

    modo oscuro v1

--- ddw-parcial-1-Higa-Navarro-Ortiz/js/main.js
+++ ddw-parcial-1-Higa-Navarro-Ortiz/js/main.js
@@ -1,1 +1,21 @@
-import { users } from "../data/users";
+
+// Agregar eventListener para cambio de theme 
+document.addEventListener('DOMContentLoaded', () => {
+    const themeSwitch = document.getElementById('themeSwitch')
+
+    themeSwitch.addEventListener('change', () => {
+        console.log('LLEGO A ESTA PARTE')
+        const main = document.querySelectorAll('main');
+        if (themeSwitch.checked) {
+          main.forEach(section => {
+            section.classList.add('dark');
+            console.log('checked classList', section.classList)
+          });
+        } else {
+          main.forEach(section => {
+            section.classList.remove('dark');
+            console.log('unchecked classList', section.classList)
+          });
+        }
+      });
+})
